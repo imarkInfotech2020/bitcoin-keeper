f930c3f144d0efbc10cecd8df6dc7b6d6044933d
cakesoft-vaibhav <vaibhav@cakesofttech.com>
2025-11-28 16:06:56 +0530

    feat: MainNetAddress

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -13,17 +13,17 @@ export const devAddress: Address = {
 };
 
 export const prodAddress: Address = {
-  settings: 'tb1q8rarxyjt9pumj8gsx770d4tmcced6ewpzjtjtp',
-  assistServer: 'tb1qtp5xngklpc9d9qajeps5zwaud0g2g5hj6tpefp',
-  inheritanceDoc: 'tb1qs58amjlnhk40frgtjysve0s2hmfem7zk6heps5',
-  health: 'tb1qs58amjlnhk40frgtjysve0s2hmfem7zk6heps5',
-  canary: 'tb1qs58amjlnhk40frgtjysve0s2hmfem7zk6heps5',
-  timeLock: 'tb1qs58amjlnhk40frgtjysve0s2hmfem7zk6heps5',
-  emergency: 'tb1qs58amjlnhk40frgtjysve0s2hmfem7zk6heps5',
-  inheritance: 'tb1qs58amjlnhk40frgtjysve0s2hmfem7zk6heps5',
-  serverKey: 'tb1qs58amjlnhk40frgtjysve0s2hmfem7zk6heps5',
-  multiSigCreate: 'tb1qs58amjlnhk40frgtjysve0s2hmfem7zk6heps5',
-  multiSigImport: 'tb1qs58amjlnhk40frgtjysve0s2hmfem7zk6heps5',
+  settings: 'bc1q3usznw6j7j32hrq594zshmrkueccvqaene9d84',
+  assistServer: 'bc1qsmpp37ddcpz58n35ka5nq0kqva2lhkzjye0z22',
+  inheritanceDoc: 'bc1qumm0fm39vz2yngx363jxgwwzpg4gms82eculd3',
+  health: 'bc1qdhwpu5jua7yls6ayryart4ahtsefh6c0lek880',
+  canary: 'bc1q5uqck8a6dnfpz7kejagsmfcctfgn4007y93wen',
+  timeLock: 'bc1q3sjawu2asxsuqhlss5f7mwnx55g43jvsup4gz6',
+  emergency: 'bc1qpajaa57xl7dt0n872ya9lhffdu747cg024myqv',
+  inheritance: 'bc1qred6ul5y3c0hydhnxheh260v06fhy60xge5umf',
+  serverKey: 'bc1q7chvxphswyp7l7fuwjvtmhr9hgfa6hd0csa7qs',
+  multiSigCreate: 'bc1q5hupxjdtvnxl3m9pwdkz9zvfe2f7n94ec425r0',
+  multiSigImport: 'bc1qwtzgguy7epug60g3pgz9zxg5qgs8g4dexw2m0d',
 };
 
 export type Address = {
